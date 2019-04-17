fa0c879794b5015ea387a0c47f75dc70ff610d7c
Nick Murphy <comfortablynick@gmail.com>
2019-04-16 22:01:56 -0500

    Add --precise option

--- src/uptime.rs
+++ src/uptime.rs
@@ -8,7 +8,8 @@ use std::{
 use systemstat::{Platform, System};
 
 fn print_help(command: &str, opts: Options) {
-    let usage = format!("Usage: {} {} [options]", super::PROG, command);
+    const DESC: &str = "Print uptime in specified format.";
+    let usage = format!("Usage: {} {} [options]\n\n{}", super::PROG, command, DESC);
     print!("{}", opts.usage(&usage));
 }
 
@@ -17,6 +18,8 @@ pub fn main(args: Vec<String>) -> Result<(), Box<std::error::Error>> {
 
     let mut opts = Options::new();
     opts.optflag("h", "help", "print this help menu");
+    opts.optflag("w", "weeks", "format as weeks instead of days");
+    opts.optflag("p", "precise", "show hours or minutes (if < 1 hour)");
 
     let matches = match opts.parse(&args[1..]) {
         Ok(m) => m,
@@ -33,14 +36,21 @@ pub fn main(args: Vec<String>) -> Result<(), Box<std::error::Error>> {
     let duration = chrono::Duration::from_std(uptime)?;
     let stdout = io::stdout();
     let mut cout = stdout.lock();
+    write!(cout, "↑")?;
     // push weeks
-    if duration.num_weeks() > 0 {
+    let fmt_weeks = matches.opt_present("weeks");
+    if duration.num_weeks() > 0 && fmt_weeks {
         write!(cout, "{}w", duration.num_weeks())?;
     }
     // push days
-    let days = duration
-        .sub(chrono::Duration::weeks(duration.num_weeks()))
-        .num_days();
+    let days = if fmt_weeks {
+        duration
+            .sub(chrono::Duration::weeks(duration.num_weeks()))
+            .num_days()
+    } else {
+        duration.num_days()
+    };
+
     if days > 0 {
         write!(cout, "{}d", days)?;
     }
@@ -48,7 +58,7 @@ pub fn main(args: Vec<String>) -> Result<(), Box<std::error::Error>> {
     let hours = duration
         .sub(chrono::Duration::days(duration.num_days()))
         .num_hours();
-    if hours > 0 && duration.num_weeks() == 0 {
+    if hours > 0 && (duration.num_weeks() == 0 || matches.opt_present("precise")) {
         write!(cout, "{}h", hours)?;
     }
     // push minutes if < 1 hour
